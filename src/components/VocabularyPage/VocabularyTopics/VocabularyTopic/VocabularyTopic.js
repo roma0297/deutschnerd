d678c58d2da981333f4cd0e893dca05ceb29ba8d
Roman Pochanin <roma_02.97@mail.ru>
2020-04-09 20:19:57 +0200

    Add routing for vocabulary topics

--- src/components/VocabularyPage/VocabularyTopics/VocabularyTopic/VocabularyTopic.js
+++ src/components/VocabularyPage/VocabularyTopics/VocabularyTopic/VocabularyTopic.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './VocabularyTopic.module.scss'
+import {withRouter} from "react-router";
 
-const vocabularyTopic = (props) => (
-    <div className={styles.VocabularyTopic}>
-        <div className={styles.VocabularyTopic__inner}>
-            <h3>{props.name || ""}</h3>
+const vocabularyTopic = (props) => {
+    const handleClick = (link) => {
+        props.history.push(link)
+    };
+
+    return(
+        <div className={styles.VocabularyTopic} onClick={() => handleClick(`/vocabulary/${props.id}`)}>
+            <div className={styles.VocabularyTopic__inner}>
+                <h3>{props.name || ""}</h3>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
-export default vocabularyTopic;
+export default withRouter(vocabularyTopic);
