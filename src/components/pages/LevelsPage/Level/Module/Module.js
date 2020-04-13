a1a050cbd5205c5ab27c3a074209e71c0592661c
Roman Pochanin <roma_02.97@mail.ru>
2020-04-13 12:01:41 +0200

    Add routing for grammar modules

--- src/components/pages/LevelsPage/Level/Module/Module.js
+++ src/components/pages/LevelsPage/Level/Module/Module.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import styles from './Modules.module.scss'
+import {withRouter} from 'react-router';
 
-const Module = (props) => (
-    <div className={styles.Module}>
-        <h3 className={styles.Module__title}>{props.name}</h3>
-        <p className={styles.Module__description}>
-            Изучаем основы HTML и CSS. На практике разбираемся с
-            семантической разметкой и базовыми механизмами
-            стилизации на примере небольшого сайта.
-        </p>
-    </div>
-);
+const Module = (props) => {
 
-export default Module;
+    const handleClick = (link) => {
+        props.history.push(link)
+    };
+
+    return (
+        <div className={styles.Module} onClick={() => handleClick(props.link)}>
+            <h3 className={styles.Module__title}>{props.name}</h3>
+            <p className={styles.Module__description}>
+                Изучаем основы HTML и CSS. На практике разбираемся с
+                семантической разметкой и базовыми механизмами
+                стилизации на примере небольшого сайта.
+            </p>
+        </div>
+    );
+};
+
+export default withRouter(Module);
